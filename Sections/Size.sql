7d383bc6362b40924d17d3a035795a9fcb8551c4
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-03-03 10:13:25 +0100

    Update Size.sql

--- Sections/Size.sql
+++ Sections/Size.sql
@@ -3,3 +3,11 @@ SELECT table_schema "DB Name",
 Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB"
 FROM information_schema.tables
 GROUP BY table_schema; 
+
+// SIZE of one table
+SELECT 
+    table_name AS `Table`, 
+    round(((data_length + index_length) / 1024 / 1024), 2) `Size in MB` 
+FROM information_schema.TABLES 
+WHERE table_schema = "bdd"
+    AND table_name = "table";
