4b958e988ccc066a5ea21e23afd9e9135a4e5d75
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-05-24 15:38:03 +0200

    Update PostByTheme.sql

--- Sections/WordPress/PostByTheme.sql
+++ Sections/WordPress/PostByTheme.sql
@@ -1,7 +1,7 @@
 SELECT *
     FROM wp_posts
     WHERE id IN (SELECT post_id 
-        FROM wp_postmeta 
+    FROM wp_postmeta 
         WHERE post_id IN (SELECT ID 
             FROM wp_posts 
             WHERE post_type = 'page')
