4879dacfd1b85820531923b4147518f42460d1c7
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-05-24 15:37:21 +0200

    Update PostByTheme.sql

--- Sections/WordPress/PostByTheme.sql
+++ Sections/WordPress/PostByTheme.sql
@@ -1,10 +1,9 @@
 SELECT *
-	FROM wp_posts
-	WHERE id IN (SELECT post_id 
-						FROM wp_postmeta 
-						WHERE post_id IN (SELECT ID 
-													FROM wp_posts 
-													WHERE post_type = 'page')
-						AND meta_key = '_wp_page_template'
-						AND meta_value = 'theme.php');
-
+    FROM wp_posts
+    WHERE id IN (SELECT post_id 
+        FROM wp_postmeta 
+        WHERE post_id IN (SELECT ID 
+            FROM wp_posts 
+            WHERE post_type = 'page')
+        AND meta_key = '_wp_page_template'
+        AND meta_value = 'theme.php');
